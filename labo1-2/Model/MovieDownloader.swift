7d30616f86a2cfcdc1035f4e98411ca3e1079763
Yassine Belkaousse <126600175+YassineGit-hub@users.noreply.github.com>
2023-08-03 19:43:57 -0400

    Update MovieDownloader.swift

--- labo1-2/Model/MovieDownloader.swift
+++ labo1-2/Model/MovieDownloader.swift
@@ -1,11 +1,17 @@
 import Foundation
 
+ 
+
 struct Movie: Codable {
     let Title: String
+
+
 }
 
+ 
+
 struct MovieDownloader {
-    let apiKey = "d30fb14f"
+    let apiKey = "7e05242e"
     let baseURL = "https://www.omdbapi.com/?apikey="
 
     func downloadMovie(withID id: String, completion: @escaping (Movie?) -> ()) {
@@ -15,28 +21,41 @@ struct MovieDownloader {
             return
         }
 
+ 
+
         URLSession.shared.dataTask(with: url) { (data, _, error) in
             guard let data = data, error == nil else {
-                print("Erreur lors du téléchargement du film")
+                print("Error downloading the movie")
                 completion(nil)
                 return
             }
-            let decoder = JSONDecoder()
-            if let movie = try? decoder.decode(Movie.self, from: data) {
-                completion(movie)
-            } else {
-                do {
-                    let movie = try decoder.decode(Movie.self, from: data)
-                    completion(movie)
-                } catch let error {
-                    print("Erreur lors de la décomposition du film: \(error)")
-                    completion(nil)
-                }
+
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("Raw JSON Response: \(jsonString)")
             }
+
+ 
+
+            let decoder = JSONDecoder()
+            do {
+                let movie = try decoder.decode(Movie.self, from: data)
+                completion(movie)
+            } catch let error {
+                print("Error decoding the movie: \(error)")
+                completion(nil)
+            }
+
+ 
 
         }.resume()
     }
+
+ 
+
 }
+
+ 
 
 let movieDownloader = MovieDownloader()
 
+ 
