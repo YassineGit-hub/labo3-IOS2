7d8b274602e2dc97748c2a22f6d060e6bdb6ef9d
Yassine <peace.org.ca@gmail.com>
2023-08-20 20:54:08 -0400

    ajout de navigator

---
+++ labo1-2/Model/jeuPenduDictionnary.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+class JeuPenduDictionnary {
+
+    private var wordToGuess: String = ""
+    private var currentWordState: [Character] = []
+    private var errorsCount: Int = 0
+
+    private let wordDownloader = WordDownloader()
+
+    func startNewGame(completion: @escaping (Bool) -> Void) {
+        wordDownloader.fetchRandomWord { [weak self] word in
+            guard let self = self, let word = word else {
+                completion(false)
+                return
+            }
+            self.initializeGame(with: word)
+            completion(true)
+        }
+    }
+
+    private func initializeGame(with word: String) {
+        self.wordToGuess = word
+        self.currentWordState = Array(repeating: "_", count: word.count)
+        self.errorsCount = 0
+    }
+
+    func guessLetter(_ letter: Character) {
+        if wordToGuess.contains(letter) {
+            for (index, wordLetter) in wordToGuess.enumerated() {
+                if wordLetter == letter {
+                    currentWordState[index] = letter
+                }
+            }
+        } else {
+            errorsCount += 1
+        }
+    }
+
+    func getCurrentState() -> String {
+        return String(currentWordState)
+    }
+
+    func getErrorsCount() -> Int {
+        return errorsCount
+    }
+    
+    func hasGameEnded() -> Bool {
+        return errorsCount >= 6 || !currentWordState.contains("_")
+    }
+}
---
+++ labo1-2/Model/wordDownloader.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class WordDownloader {
+
+    private let apiUrlString = "https://random-word-api.herokuapp.com/word"
+    
+    func fetchRandomWord(completion: @escaping (String?) -> Void) {
+        guard let url = URL(string: apiUrlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, error == nil {
+                do {
+                    if let words = try JSONSerialization.jsonObject(with: data, options: []) as? [String], let word = words.first {
+                        completion(word)
+                    } else {
+                        completion(nil)
+                    }
+                } catch {
+                    completion(nil)
+                }
+            } else {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
